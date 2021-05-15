ba9f0178c84029bafd3e6b3e5d4d7355d19babaf
sjpriya <72385035+sjpriya@users.noreply.github.com>
2021-05-15 21:15:33 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -17,17 +17,16 @@ class Game {
 
   async start(){
     if(gameState === 0){
-      player = new Player();
-      var playerCountRef = await database.ref('playerCount').once("value");
-      if(playerCountRef.exists()){
-        playerCount = playerCountRef.val();
-        player.getCount();
-      }
-      form = new Form();
-      form.display();
+      // create new player
+      
+      // if playerCountRef exists, get count 
+      
+      // create new form and display it
+      
     }
   }
 
   play(){
+    // display game start using text function
   }
 }
